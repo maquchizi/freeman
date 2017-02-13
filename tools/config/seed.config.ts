7650a6be22869cc221511e939ddebc34ab8ba814
Mark Ng'ang'a <maqnganga@gmail.com>
2017-02-13 23:08:52 +0300

    Update project
     * Change title
     * Add repo to package.json

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -144,7 +144,7 @@ export class SeedConfig {
    * `index.html`.
    * @type {string}
    */
-  APP_TITLE = 'Welcome to angular-seed!';
+  APP_TITLE = 'Freeman';
 
   /**
    * The base folder of the applications source files.
