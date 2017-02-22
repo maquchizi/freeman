29e4e0d32da6a62078b2ebee41907025e8737fee
Mark Ng'ang'a <maqnganga@gmail.com>
2017-02-22 18:25:28 +0300

    Clean up the home component

--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -2,10 +2,8 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NameListService } from './name-list/name-list.service';
 import { AuthService } from './auth/auth.service';
 
 /**
@@ -22,7 +20,7 @@ export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [NameListService, AuthService]
+      providers: [AuthService]
     };
   }
 }
